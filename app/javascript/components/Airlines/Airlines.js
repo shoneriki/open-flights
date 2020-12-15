ec374acae9aac03349c217edc9945f06ac540fde
shoneriki <ikirenohs@gmail.com>
2020-12-15 21:50:58 +0900

    changed formatting of airlines index

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -20,10 +20,18 @@ const Airlines = () => {
   })
 
   return (
-    <Fragment>
-      <div>This is the Airlines#index view for our app</div>
-      <ul>{list}</ul>
-    </Fragment>
+    <div className="home">
+
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest, unbiased airline reviews.</div>
+      </div>
+
+      <div className="grid">
+        <ul>{list}</ul>
+      </div>
+
+    </div>
   )
 }
 
