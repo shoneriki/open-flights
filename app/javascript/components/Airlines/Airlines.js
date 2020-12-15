b09c05a3b920972c6e3800ffae19e39e6e97ea62
shoneriki <ikirenohs@gmail.com>
2020-12-15 21:46:55 +0900

    airlines index shows list of airlines after debugging

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
@@ -7,11 +8,23 @@ const Airlines = () => {
     // Get all airlines from api
     // Update airlines in our state
 
+    axios.get('/api/v1/airlines.json')
+    .then( resp => {
+      setAirlines(resp.data.data)
+    })
+    .catch( resp => console.log(resp) )
+  }, [airlines.length])
+
+  const list = airlines.map( item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
   })
 
   return (
-    <div>This is the Airlines#index view for our app</div>
-    )
+    <Fragment>
+      <div>This is the Airlines#index view for our app</div>
+      <ul>{list}</ul>
+    </Fragment>
+  )
 }
 
 export default Airlines
