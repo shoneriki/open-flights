a75bc161b33ef8a0629dcef7969a4fa14def2582
shoneriki <ikirenohs@gmail.com>
2020-12-15 21:12:25 +0900

    added axios

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Airlines = () => {
-  return <div>This is the Airlines#index view for our app</div>
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    // Get all airlines from api
+    // Update airlines in our state
+
+  })
+
+  return (
+    <div>This is the Airlines#index view for our app</div>
+    )
 }
 
 export default Airlines
