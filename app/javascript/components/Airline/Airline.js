a26915ef44c43b053a980141560b179cc21da647
shoneriki <ikirenohs@gmail.com>
2020-12-16 09:52:44 +0900

    same code, but doesn't work for show page

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Header from './Header'
 
-const Airline = () => {
-  return <div>This is the Airlines#show view for our app</div>
+const Airline = (props) => {
+  const {airline, setAirline} = useState({})
+  const {review, setReview} = useState({})
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(()=>{
+    const slug = props.match.params.slug
+    const url = `/api/v1/airlines/${slug}`
+
+    axios.get(url)
+    .then( resp => {
+      setAirline(resp.data)
+      setLoaded(true)
+    })
+    .catch( resp => console.log(resp) )
+  }, [])
+
+  return (
+    <div className="wrapper">
+      <div className="column">
+        {
+          loaded &&
+          <Header
+            attributes={airline.data.attributes}
+            reviews={airline.included}
+          />
+        }
+    <div className="reviews"></div>
+    </div>
+    <div className="column">
+      <div className="review-form">[Review form goes here]</div>
+    </div>
+  </div>
+  )
 }
 
 export default Airline
