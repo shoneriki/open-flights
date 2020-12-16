98929a0a05582d22beeb28c3f6be0b7e9633f168
shoneriki <ikirenohs@gmail.com>
2020-12-16 14:59:51 +0900

    airlines show page styling

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,28 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Header from './Header'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+  background: #fff;
+  height: 100vh;
+  overflow: scroll;
+
+  &:last-child {
+    background: #000;
+
+  }
+`
+const Main = styled.div`
+  left-padding: 50px;
+`
+
 
 const Airline = (props) => {
   const {airline, setAirline} = useState({})
@@ -22,21 +44,23 @@ const Airline = (props) => {
 
 
   return (
-    <div className="wrapper">
-      <div className="column">
-        {
-          loaded &&
-          <Header
-            attributes={airline.data.attributes}
-            reviews={airline.included}
-          />
-        }
-    <div className="reviews"></div>
-    </div>
-    <div className="column">
-      <div className="review-form">[Review form goes here]</div>
-    </div>
-  </div>
+    <Wrapper>
+      <Column>
+        <Main>
+          {
+            loaded &&
+            <Header
+              attributes={airline.data.attributes}
+              reviews={airline.included}
+            />
+          }
+          <div className="reviews"></div>
+        </Main>
+      </Column>
+      <Column>
+        <div className="review-form">[Review form goes here]</div>
+      </Column>
+    </Wrapper>
   )
 }
 
