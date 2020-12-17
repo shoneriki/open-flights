9763348a1e281d46af15dc0f9e39678fea5abe77
shoneriki <ikirenohs@gmail.com>
2020-12-17 12:22:10 +0900

    airline/review.js destructuring

--- app/javascript/components/Airline/Review.js
+++ app/javascript/components/Airline/Review.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const Review = (props) => {
-  
+  const {score, title, description} = props.attributes
+
   return (
     <div className="card">
       <div className="rating-container">
-        <div className="rating-score"></div>
+        <div className="rating-score">{score}</div>
       </div>
-      <div className="title"></div>
-      <div className="description"></div>
+      <div className="title">{title}</div>
+      <div className="description">{description}</div>
     </div>
   )
 }
