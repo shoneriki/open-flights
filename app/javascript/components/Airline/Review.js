d1b3e255196bb20e24e8aee99c63df92923a5604
shoneriki <ikirenohs@gmail.com>
2020-12-17 12:14:00 +0900

    imported review into airline.js

--- app/javascript/components/Airline/Review.js
+++ app/javascript/components/Airline/Review.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Review = (props) => {
-
+  return (
+    <div className="card">
+      <div className="rating-container">
+        <div className="rating-score"></div>
+      </div>
+      <div className="title"></div>
+      <div className="description"></div>
+    </div>
+  )
 }
 
 export default Review
